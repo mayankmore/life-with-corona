0a3cc1af1d90fc07b4d28b33cd5531e605996d08
lakshyaj21 <lakshyaj21@gmail.com>
2021-04-10 20:46:00 +0530

    prediction form updated

--- life-with-corona/src/Components/LabComponents/Prediction.js
+++ life-with-corona/src/Components/LabComponents/Prediction.js
@@ -1,7 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './materialize.css'
 
+async function loginUser(credentials) {
+ return fetch('http://localhost:5000/prediction', {
+   method: 'POST',
+   headers: {
+     'Content-Type': 'application/json'
+   },
+   body: JSON.stringify(credentials)
+ })
+   .then(data => data.json).then(data =>
+       setPrediction(data.prediction)
+   );
+
+}
+
 function Prediction(){
+    const [Age, setAge] = useState();
+    const [prediction, setPrediction] = useState();
+
+    const handleSubmit = async e => {
+        e.preventDefault();
+        const token = await loginUser({
+          username,
+          password
+        });
+  }
 return(
 <>
 <img src="https://ak.picdn.net/shutterstock/videos/10776032/thumb/1.jpg" class="im"/>
@@ -9,14 +33,14 @@ return(
   <div class="title">
     <h1>Classifier</h1>
 </div>
-  <div class="contact-form">
+  <form class="contact-form" >
     <div class="input-fields">
-      <input type="text" class="input" placeholder="Name"/>
-      <input type="text" class="input" placeholder="Email Address"/>
-      <input type="text" class="input" placeholder="Phone"/>
-      <input type="text" class="input" placeholder="Subject"/>
-      <input type="text" class="input" placeholder="Phone"/>
-      <input type="text" class="input" placeholder="Subject"/>
+      <input type="text" class="input" placeholder="Age" onChange={e => setAge(e.target.value)}/>
+      <input type="text" class="input" placeholder="Email Address" onChange={e => setUserName(e.target.value)}/>
+      <input type="text" class="input" placeholder="Phone" onChange={e => setUserName(e.target.value)}/>
+      <input type="text" class="input" placeholder="Subject" onChange={e => setUserName(e.target.value)}/>
+      <input type="text" class="input" placeholder="Phone" onChange={e => setUserName(e.target.value)}/>
+      <input type="text" class="input" placeholder="Subject" onChange={e => setUserName(e.target.value)}/>
 
     </div>
     <div class="input-fields">
@@ -26,29 +50,14 @@ return(
       <input type="text" class="input" placeholder="Subject"/>
       <input type="text" class="input" placeholder="Phone"/>
       <input type="text" class="input" placeholder="Subject"/>
-      
     </div>
-    
-  </div>
-  <div class="button">  <a href="#" class="btnx btn3">Predict</a>  </div>
-
-
-
+    <div class="button" onSubmit={handleSubmit}>  <a href="#" class="btnx btn3">Predict</a>  </div>
+  </form>
 
 </div>
 
-
-
-
-
-
 </>
 )
-
-
-
-
-
 
 }
 export default Prediction

6c108ee1184f404e685133c4048e2f9471678e0d
Mayank <moremayank993@gmail.com>
2021-04-10 20:16:49 +0530

    lab prediction created

--- life-with-corona/src/Components/LabComponents/Prediction.js
+++ life-with-corona/src/Components/LabComponents/Prediction.js
@@ -1,108 +1,54 @@
-import React from 'react';
+import React from 'react'
 import './materialize.css'
-function Prediction (){
-    return(
-        <div class="container">
-        <br/><br/>
-        <h1 class="header center orange-text">Corona Prediction</h1>
-        <div class="row center">
-          <h5 class="header col s12 light">Predict the probability of Corona on basis of symptoms
-          <br/>
-          </h5>
-        </div>
-  
-                <div class="row">
-      <form action='/predict' method="post" class="col s12">
-        <div class="row">
-          <div class="input-field col s4">
-              <label for="first_name"><b>Temperature</b></label>
-              <br/>
-            <input placeholder="Temperature in Celsius" name="Temperature" id="first_name" type="text" class="validate" />
-          </div>
-          <div class="input-field col s4">
-  
-            <label for="last_name"><b>Oxygen </b></label>
-              <br/>
-            <input id="last_name" name="Oxygen" placeholder="Oxygen content in ppm"  type="text" class="validate" />
-  
-          </div>
-  
-          <div class="input-field col s4">
-              <label for="_name"><b>Humidity</b></label>
-              <br/>
-            <input id="_name" name="Humidity" placeholder="Humidity %" type="text" class="validate" /> 
-  
-          </div>
-          <div class="input-field col s4">
-            <label for="first_name"><b>Temperature</b></label>
-            <br/>
-          <input placeholder="Temperature in Celsius" name="Temperature" id="first_name" type="text" class="validate" />
-        </div>
-        <div class="input-field col s4">
 
-          <label for="last_name"><b>Oxygen </b></label>
-            <br/>
-          <input id="last_name" name="Oxygen" placeholder="Oxygen content in ppm"  type="text" class="validate" />
-
-        </div>
-
-        <div class="input-field col s4">
-            <label for="_name"><b>Humidity</b></label>
-            <br/>
-          <input id="_name" name="Humidity" placeholder="Humidity %" type="text" class="validate" />
-
-        </div>
-        <div class="input-field col s4">
-          <label for="first_name"><b>Temperature</b></label>
-          <br/>
-        <input placeholder="Temperature in Celsius" name="Temperature" id="first_name" type="text" class="validate" />
-      </div>
-      <div class="input-field col s4">
-
-        <label for="last_name"><b>Oxygen </b></label>
-          <br/>
-        <input id="last_name" name="Oxygen" placeholder="Oxygen content in ppm"  type="text" class="validate" />
-
-      </div>
-
-      <div class="input-field col s4">
-          <label for="_name"><b>Humidity</b></label>
-          <br/>
-        <input id="_name" name="Humidity" placeholder="Humidity %" type="text" class="validate" />
-
-      </div>
-      <div class="input-field col s4">
-        <label for="first_name"><b>Temperature</b></label>
-        <br/>
-      <input placeholder="Temperature in Celsius" name="Temperature" id="first_name" type="text" class="validate" />
-    </div>
-    <div class="input-field col s4">
-
-      <label for="last_name"><b>Oxygen </b></label>
-        <br/>
-      <input id="last_name" name="Oxygen" placeholder="Oxygen content in ppm"  type="text" class="validate" />
+function Prediction(){
+return(
+<>
+<img src="https://ak.picdn.net/shutterstock/videos/10776032/thumb/1.jpg" class="im"/>
+<div class="wrapper1">
+  <div class="title">
+    <h1>Classifier</h1>
+</div>
+  <div class="contact-form">
+    <div class="input-fields">
+      <input type="text" class="input" placeholder="Name"/>
+      <input type="text" class="input" placeholder="Email Address"/>
+      <input type="text" class="input" placeholder="Phone"/>
+      <input type="text" class="input" placeholder="Subject"/>
+      <input type="text" class="input" placeholder="Phone"/>
+      <input type="text" class="input" placeholder="Subject"/>
 
     </div>
-
-    <div class="input-field col s4">
-        <label for="_name"><b>Humidity</b></label>
-        <br/>
-      <input id="_name" name="Humidity" placeholder="Humidity %" type="text" class="validate" />
-
+    <div class="input-fields">
+      <input type="text" class="input" placeholder="Name"/>
+      <input type="text" class="input" placeholder="Email Address"/>
+      <input type="text" class="input" placeholder="Phone"/>
+      <input type="text" class="input" placeholder="Subject"/>
+      <input type="text" class="input" placeholder="Phone"/>
+      <input type="text" class="input" placeholder="Subject"/>
+      
     </div>
-  
-        </div>
-  
-        <div class="row center">
-  
-            <button type="submit" class="btn-large waves-effect waves-light orange">Predict Probability</button>
-        </div>
-      </form>
-        </div>
-  
-      </div>
     
-    )
-}
+  </div>
+  <div class="button">  <a href="#" class="btnx btn3">Predict</a>  </div>
 
+
+
+
+</div>
+
+
+
+
+
+
+</>
+)
+
+
+
+
+
+
+}
 export default Prediction
--- life-with-corona/src/Components/LabComponents/lab.js
+++ life-with-corona/src/Components/LabComponents/lab.js
@@ -4,13 +4,13 @@ import './lab.css'
 function Lab() {
 return(
 
-
+   <div class = "bckimg">
     <div class="middle">
     <div class="headdiv"><h1 class="headone">LAUDA LASSAN!</h1></div>
     <Link to="/xray" class="btnz btn1">X-Ray Prediction</Link>
     <Link to="/prediction" class="btnz btn2">ML Classifier</Link>
   </div>
-
+  </div>
 
 
 
